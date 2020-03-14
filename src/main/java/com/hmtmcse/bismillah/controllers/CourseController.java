c129d32d7ccbdbcc57d0868febdcbc4ab74c89f6
Touhid Mia <hmtm.cse@gmail.com>
2020-03-14 15:03:27 +0600

    Course CRUD

--- src/main/java/com/hmtmcse/bismillah/controllers/CourseController.java
+++ src/main/java/com/hmtmcse/bismillah/controllers/CourseController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -20,6 +21,31 @@ public class CourseController {
         }
         model.addAttribute("title", title);
         return "course/form";
+    }
+
+    @PostMapping("/create")
+    public String create(){
+        return "redirect:/form";
+    }
+
+    @GetMapping("/readDetails")
+    public String readDetails(){
+        return "redirect:/form";
+    }
+
+    @GetMapping("/readList")
+    public String readList(){
+        return "redirect:/form";
+    }
+
+    @PostMapping("/update")
+    public String update(){
+        return "redirect:/form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(){
+        return "redirect:/form";
     }
 
 }
