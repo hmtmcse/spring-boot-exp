7016ae8f55349b009042f700cfab92f97a6fec39
Touhid Mia <hmtm.cse@gmail.com>
2020-03-26 22:06:59 +0600

    URL Mapping With Regex

--- src/main/java/com/hmtmcse/bismillah/controllers/UrlMappingController.java
+++ src/main/java/com/hmtmcse/bismillah/controllers/UrlMappingController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/url-mapping")
 public class UrlMappingController {
@@ -21,6 +23,18 @@ public class UrlMappingController {
     @ResponseBody
     public String isIdString(@PathVariable("id") String id){
         return "The ID is String " + id;
+    }
+
+    @RequestMapping("/check/{id:^(?!asset).*}/**")
+    @ResponseBody
+    public String checkIsNotRest(@PathVariable("id") String id, HttpServletRequest request){
+        return "Check then not Rest then Anything  -- " + id + " --- " + request.getRequestURI();
+    }
+
+    @RequestMapping("/check/rest/**")
+    @ResponseBody
+    public String checkRestAfterAnyThing(){
+        return "Check then Rest then Anything ";
     }
 
 }
