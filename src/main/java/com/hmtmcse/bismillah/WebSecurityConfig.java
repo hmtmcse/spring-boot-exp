4e041f57b0774f878b9af23aa7f2b2cfa7ffd627
Touhid Mia <hmtm.cse@gmail.com>
2020-05-20 03:05:25 +0600

    spring security

--- src/main/java/com/hmtmcse/bismillah/WebSecurityConfig.java
+++ src/main/java/com/hmtmcse/bismillah/WebSecurityConfig.java
@@ -17,7 +17,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/home")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
